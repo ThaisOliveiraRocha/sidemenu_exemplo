f0e82f0a1369630008a41630b0ce27004f5595b8
Thais Rocha <thais.oliveirarocha@hotmail.com>
2020-01-30 16:40:27 -0300

    Primeira versao do side menu

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+
+// Be sure to include styles at some point, probably during your bootstraping
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SideNav
+          onSelect={(selected) => {
+              // Add your code here
+          }}
+      >
+    <SideNav.Toggle />
+    <SideNav.Nav defaultSelected="home">
+        <NavItem eventKey="home">
+            <NavIcon>
+                <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+            </NavIcon>
+            <NavText>
+                Home
+            </NavText>
+        </NavItem>
+        <NavItem eventKey="charts">
+            <NavIcon>
+                <i className="fa fa-fw fa-line-chart" style={{ fontSize: '1.75em' }} />
+            </NavIcon>
+            <NavText>
+                Charts
+            </NavText>
+            <NavItem eventKey="charts/linechart">
+                <NavText>
+                    Line Chart
+                </NavText>
+            </NavItem>
+            <NavItem eventKey="charts/barchart">
+                <NavText>
+                    Bar Chart
+                </NavText>
+            </NavItem>
+        </NavItem>
+    </SideNav.Nav>
+</SideNav>
+
+
+
+    </div>
+  );
+}
+
+export default App;
